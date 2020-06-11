fa1bdadd8410d8fb66f6733c5b5c6d4946f018c8
Milo Rupp <badtzmaru139@gmail.com>
2020-06-11 14:50:10 -0700

    Basic commands, html support

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ var port = process.env.PORT || 80;
 var userCount = 0;
 var isRefreshed = false;
 
+var links = [
+  {
+    command: '!attendance',
+    link: '<a href="attendance.pscs.org">Attendance System</a>',
+  },
+  {
+    command: '!manifesto',
+    link: '<a href="https://docs.google.com/document/d/1sAaruzhYcoHRRGTIGUbJcbn1MFx_zi3x4Urf1En2Ty4">Manifesto</a>',
+  },
+  {
+    command: '!pscsclicker',
+    link: '<a href="https://coderlads.github.io/pscsclicker/">PSCS Clicker</a>',
+  }
+];
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -15,6 +30,12 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   socket.on('chat message', function (msg, color) {
+    links.forEach(item => {
+      if(item.command === msg){
+        msg = item.link;
+      }
+    });
+
     io.emit('chat message', msg, color);
   });
 });
@@ -26,8 +47,8 @@ setInterval(() => {
   if (io.engine.clientsCount !== userCount) {
     userCount = io.engine.clientsCount;
     io.emit('user count', userCount);
-    if(!isRefreshed){
-      io.emit('refresh',{});
+    if (!isRefreshed) {
+      io.emit('refresh', {});
       isRefreshed = true;
     }
   }
