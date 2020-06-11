bf1c861fc6b2b9069de330559bd3e0d4b6a4b6c4
Milo Rupp <badtzmaru139@gmail.com>
2020-06-11 13:46:32 -0700

    add usercount

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 80;
 
+var userCount = 0;
+
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg,color){
-    io.emit('chat message', msg,color);
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg, color) {
+    io.emit('chat message', msg, color);
   });
 });
 
-http.listen(port, function(){
+setInterval(() => {
+
+  if (io.engine.clientsCount !== userCount) {
+    userCount = io.engine.clientsCount;
+    io.emit('user count', userCount);
+  }
+
+}, 1000);
+
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
