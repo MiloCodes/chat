9a76533f9bc44d67f3e8e649c278710c1369503e
riverrrrrr <riverehill@gmail.com>
2020-06-12 00:10:48 -0700

    added discord rpc

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow } = require('electron')
+const DiscordRPC = require('discord-rpc');
+const keys = require(__dirname + '/keys.json');
+
+let win;
 
 function createWindow () {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: true,
@@ -15,3 +19,42 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow)
+
+const clientId = keys.discordId;
+
+DiscordRPC.register(clientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc || !win) {
+    return;
+  }
+
+  let currentUsers = win.title.substring(6, 7);
+  let userUsers = currentUsers > 2 ? 'users' : 'user';
+  let stateMessage = currentUsers > 1 ? `with ${currentUsers - 1} ${userUsers}` : 'alone';
+
+  rpc.setActivity({
+    details: 'Currently chatting',
+    state: stateMessage,
+    startTimestamp,
+    largeImageKey: 'logo',
+    largeImageText: 'Chatting anonymously',
+    smallImageKey: 'anon',
+    // smallImageText: '',
+    instance: false,
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
