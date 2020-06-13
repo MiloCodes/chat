f4f4a653758c0afa8d46c9455524056f8ce545a4
riverrrrrr <riverehill@gmail.com>
2020-06-12 18:41:43 -0700

    temperary rpc fix

--- electron-app/main.js
+++ electron-app/main.js
@@ -32,27 +32,27 @@ async function setActivity() {
   if (!rpc || !win) {
     return;
   }
-  if (win.title) {
-    let currentUsers = win.title.substring(6, 7);
-    let userUsers = currentUsers > 2 ? 'users' : 'user';
-    let stateMessage = currentUsers > 1 ? `with ${currentUsers - 1} ${userUsers}` : 'alone';
+  
+  // let currentUsers = win.title.substring(6, 7);
+  // let userUsers = currentUsers > 2 ? 'users' : 'user';
+  // let stateMessage = currentUsers > 1 ? `with ${currentUsers - 1} ${userUsers}` : 'alone';
 
-    rpc.setActivity({
-      details: 'Currently chatting',
-      state: stateMessage,
-      startTimestamp,
-      largeImageKey: 'logo',
-      largeImageText: 'Chatting anonymously',
-      smallImageKey: 'anon',
-      // smallImageText: '',
-      instance: false,
-    });
-  }
+  rpc.setActivity({
+    details: 'Currently chatting',
+    // state: stateMessage,
+    startTimestamp,
+    largeImageKey: 'logo',
+    largeImageText: 'Chatting anonymously',
+    smallImageKey: 'anon',
+    // smallImageText: '',
+    instance: false,
+  });
+  
 }
 
 rpc.on('ready', () => {
   setActivity();
-
+  
   // activity can only be set every 15 seconds
   setInterval(() => {
     setActivity();
